7167a5c4f5eafd8c1d38293f996af3dbbc08731d
Khali-repos <118523057+Khali-repos@users.noreply.github.com>
2022-11-24 17:31:25 +0000

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from streamlit_option_menu import option_menu
 
 #saved models
 
-diabetes_model = pickle.load(open('C:/Users/Iyama/Desktop/ML/MLdiabetes/diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
 
 
 def diabetes_prediction(input_data):
